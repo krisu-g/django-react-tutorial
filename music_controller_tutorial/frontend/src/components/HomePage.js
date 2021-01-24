e33355a2a8f9ef7087c7f141b288a35f3170ded4
krisu-g <grabowski.krzysztof@gmail.com>
2021-01-24 21:22:50 +0100

    main.js...

--- music_controller_tutorial/frontend/src/components/HomePage.js
+++ music_controller_tutorial/frontend/src/components/HomePage.js
@@ -13,7 +13,7 @@ export default class HomePage extends Component {
         return (<Router>
             <Switch>
                 <Route exact path='/'>
-                    <p>This is the home page 1</p>
+                    <p>This is the home page</p>
                 </Route>
                 <Route path='/join' component={RoomJoinPage} />
                 <Route path='/create' component={CreateRoomPage} />
